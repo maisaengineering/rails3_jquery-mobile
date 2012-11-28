6f2fd7118b5c29a5304aadb9cb4abf2cd912718a
Maisa Engineering <contact@maisasolutions.com>
2012-11-28 10:58:34 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3JqueryMobile::Application.config.secret_token = 'c85f66afabbd2a624c0949793ab3636c6fd5ce93f6e9dbd7cec52855ba6177cf2a9fe174c4ca32bb6a1faacd4ed47e8bd19a2de3f846eeb972350f5e3d8c1a50'
